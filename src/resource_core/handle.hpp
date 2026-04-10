17480afd69a283e860fde7b83f952f8e6a07a165
Фролова Алина <alfrolova07@mail.ru>
2026-04-10 21:18:20 +0300

    lab4

---
+++ src/resource_core/handle.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <cstdio>
+#include <string>
+namespace lab4::resource
+{
+class FileHandle
+{
+    FILE* file_;
+
+  public:
+    FileHandle();
+    FileHandle(const std::string& name, const std::string& mode);
+    ~FileHandle();
+
+    FileHandle(const FileHandle&) = delete;
+    FileHandle& operator=(const FileHandle&) = delete;
+
+    FileHandle(FileHandle&& other) noexcept;
+    FileHandle& operator=(FileHandle&& other) noexcept;
+
+    bool is_open() const;
+    void close();
+    void open(const std::string& name, const std::string& mode);
+};
+} // namespace lab4::resource
